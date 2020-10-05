8967d1a801556462e8d9657f016d5b4e6d287b0c
Maya <mayarin@users.noreply.github.com>
2020-10-05 15:07:42 +0900

    20201005

--- public/app.js
+++ public/app.js
@@ -1,33 +1,156 @@
 firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
-    console.log(user);
-    var body = 'displayName : ' + user.displayName + '<br>' +
-      'email : ' + user.email + '<br>' +
-      'photoURL : ' + user.photoURL + '<br>' +
-      'emailVerified : ' + user.emailVerified + '<br>' +
-      'uid : ' + user.uid  + '<br>';
+    console.log(user.displayName);
+    console.log(user.email);
+    console.log(user.photoURL);
+    console.log(user.emailVerified);
+    console.log(user.uid);
 
-    document.getElementById('user-email').innerHTML = user.email
-    document.getElementById('logined').className = 'd-block';
-    document.getElementById('no_login').className = 'd-none';
+    if(user.displayName == null) {
+      getElemID('welcome').innerHTML = user.email;
+    } else {
+      getElemID('welcome').innerHTML = user.displayName;
+    }
+
+    getElemID('update_displayName').innerHTML = user.displayName;
+    // getElemID('update_now_email').innerHTML = user.displayName;
+
+    getElemID('logined').className = 'd-block';
+    getElemID('no_login').className = 'd-none';
   } else {
-    document.getElementById('logined').className = 'd-none';
-    document.getElementById('no_login').className = 'd-block';
+    getElemID('logined').className = 'd-none';
+    getElemID('no_login').className = 'd-block';
   }
 });
 
-document.querySelector("#reset_password_form").addEventListener("submit", function(event) {
-  var auth = firebase.auth();
-  var emailAddress = getElementValue("reset_password_email");
+firebase.auth().languageCode = 'ja';
 
-  auth.sendPasswordResetEmail(emailAddress).then(function() {
-    alert('パスワード変更メールを発信しました。');
+document.querySelector("#update_form").addEventListener("submit", function(event) {
+  var user = firebase.auth().currentUser;
+
+  user.updateProfile({
+    displayName : getElementValue('update_displayName')
+  }).then(function() {
+    alert('ニックネームを変更しました。');
+    location.reload();
+  }).catch(function(error) {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    alert(errorCode + ', ' + errorMessage);
+  });
+  event.preventDefault();
+}, false);
+
+document.querySelector("#update_mailaddress_form").addEventListener("submit", function(event) {
+
+  var user = firebase.auth().currentUser;
+  var credential = firebase.auth.EmailAuthProvider.credential(
+    user.email,
+    getElementValue('update_email_password')
+  );
+
+  user.reauthenticateWithCredential(credential).then(function() {
+    // User re-authenticated.
+    user.updateEmail(getElementValue('update_new_email')).then(function() {
+      user.sendEmailVerification().then(function() {
+        alert('メールアドレスを変更しました。再度ログインしてください。なお認証メールを送信しております。');
+        firebase.auth().signOut();
+      }).catch(function(error) {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        alert(errorCode + ', ' + errorMessage);
+      });
+    }).catch(function(error) {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      alert(errorCode + ', ' + errorMessage);
+    });
+
   }).catch(function(error) {
     var errorCode = error.code;
     var errorMessage = error.message;
     alert(errorCode + ', ' + errorMessage);
   });
 
+  event.preventDefault();
+}, false);
+
+document.querySelector("#update_password_form").addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  var user = firebase.auth().currentUser;
+  var credential = firebase.auth.EmailAuthProvider.credential(
+    user.email,
+    getElementValue('update_now_password')
+  );
+
+  user.reauthenticateWithCredential(credential).then(function() {
+    // User re-authenticated.
+    user.updatePassword(getElementValue('update_new_password')).then(function() {
+      alert('パスワードを変更しました。再度ログインしてください。');
+      firebase.auth().signOut();
+    }).catch(function(error) {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      alert(errorCode + ', ' + errorMessage);
+    });
+
+  }).catch(function(error) {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    alert(errorCode + ', ' + errorMessage);
+  });
+
+  event.preventDefault();
+}, false);
+
+document.querySelector("#remove_account_form").addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  var user = firebase.auth().currentUser;
+  var credential = firebase.auth.EmailAuthProvider.credential(
+    user.email,
+    getElementValue('remove_account_password')
+  );
+
+  user.reauthenticateWithCredential(credential).then(function() {
+    // User re-authenticated.
+
+    user.delete().then(function() {
+      alert('アカウントを削除しました。ご利用ありがとうございました。');
+      firebase.auth().signOut();
+    }).catch(function(error) {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      alert(errorCode + ', ' + errorMessage);
+    });
+
+  }).catch(function(error) {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    alert(errorCode + ', ' + errorMessage);
+  });
+
+  event.preventDefault();
+}, false);
+
+
+
+
+
+
+document.querySelector("#reset_password_form").addEventListener("submit", function(event) {
+  var auth = firebase.auth();
+  var emailAddress = getElementValue("reset_password_email");
+
+  auth.sendPasswordResetEmail(emailAddress).then(function() {
+    alert('パスワード変更リンクを送信しました。');
+    firebase.auth().signOut();
+  }).catch(function(error) {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    alert(errorCode + ', ' + errorMessage);
+  });
   event.preventDefault();
 }, false);
 
@@ -76,5 +199,9 @@ document.querySelector("#logout_form").addEventListener("submit", function(event
 }, false);
 
 function getElementValue(id){
-  return document.getElementById(id).value;
+  return getElemID(id).value;
+}
+
+function getElemID(id){
+  return document.getElementById(id);
 }
